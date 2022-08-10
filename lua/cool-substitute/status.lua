317aa104f186811bd909d78bea9c7f0a0a23a275
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2022-08-10 21:52:17 +0100

    fix statusline

--- lua/cool-substitute/status.lua
+++ lua/cool-substitute/status.lua
@@ -2,9 +2,9 @@ local M = {}
 
 function M.status_with_icons()
   if vim.g.cool_substitute_is_active then
-    return " Writng Substitution for " .. vim.g.cool_substitute_last_searched_word
-  elseif vim.g.cool_substitute_is_substituing then
     return " Applying substitution for " .. vim.g.cool_substitute_last_searched_word
+  elseif vim.g.cool_substitute_is_substituing then
+    return " Writng Substitution for " .. vim.g.cool_substitute_last_searched_word
   else
     return ""
   end
@@ -12,9 +12,9 @@ end
 
 function M.status_no_icons()
   if vim.g.cool_substitute_is_active then
-    return "Writng Substitution for " .. vim.g.cool_substitute_last_searched_word
-  elseif vim.g.cool_substitute_is_substituing then
     return "Applying substitution for " .. vim.g.cool_substitute_last_searched_word
+  elseif vim.g.cool_substitute_is_substituing then
+    return "Writng Substitution for " .. vim.g.cool_substitute_last_searched_word
   else
     return ""
   end
